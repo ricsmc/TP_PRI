f23268406766f1e59977f08ec4e47eef88fc9284
ricsmc <ricardosmcunha23@gmail.com>
2021-01-09 23:54:48 +0000

    Index lista JSON

--- Controller/routes/index.js
+++ Controller/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var json = [{id:'d9da0wdu', name:'Express',user:'jrc' ,date:'2020', rating:2}, { id:'d94a0wdu',user:'jcr' ,name:'Node', date:'2020', rating:2}];
+  res.render('index', {posts:json});
 });
 
 module.exports = router;
